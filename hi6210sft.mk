a9f0f5ca4f322440de6918eeb1d2bfcd9af6b95d
hak86 <hakamelassouad@gmail.com>
2016-02-18 21:44:53 +0100

    P8L: enable SRC_TARGET_DIR

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, build/target/product/full.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 $(call inherit-product-if-exists, vendor/huawei/hi6210sft/hi6210sft-vendor.mk)
 

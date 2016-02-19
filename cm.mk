c91718f0f5929e715ee7ebbbb1fe77c8d84a6c0e
hak86 <hakamelassouad@gmail.com>
2016-02-19 21:13:42 +0100

    P8L: add fingerprint settings

--- cm.mk
+++ cm.mk
@@ -15,3 +15,10 @@ PRODUCT_NAME := cm_hi6210sft
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := ALE-L21
 PRODUCT_MANUFACTURER := Huawei
+
+# Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hi6210sft \
+    TARGET_DEVICE=hi6210sft \
+    BUILD_FINGERPRINT="hi6210sft/hi6210sft/hi6210sft:5.0/LRX21M/jenkins11170249:user/test-keys" \
+    PRIVATE_BUILD_DESC="hi6210sft-user 5.0 LRX21M eng.jenkins.20151117.024706 test-keys"

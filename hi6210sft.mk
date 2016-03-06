19f8dfecde3c8358525efd055148024612afb767
hak86 <hakamelassouad@gmail.com>
2016-03-06 23:44:12 +0100

    P8L: some cleaning

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -1,7 +1,6 @@
 # Android Open Source Project Base System
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Android Packages

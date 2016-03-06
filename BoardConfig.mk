25cdd3347844b76b69ea6698e9a4714b7ce373f2
hak86 <hakamelassouad@gmail.com>
2016-03-06 11:01:26 +0100

    P8L: cleaup, in next commit i'll switch from prebuilt kernel to compile it from proper source.

---
+++ BoardConfig.mk
@@ -0,0 +1,127 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/huawei/hi6210sft/BoardConfigVendor.mk
+
+# 64 Bit
+ANDROID_64=true
+TARGET_USES_64_BIT_BINDER := true
+TARGET_IS_64_BIT := true
+TARGET_USES_HISI_DTIMAGE := true
+TARGET_SUPPORTS_32_BIT_APPS := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+
+# ADB
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
+ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mass_storage
+
+# Architecture
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT_FPU := neon
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := generic
+
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+#BOARD_USES_GENERIC_AUDIO := false
+
+# Board
+BOARD_HAS_NO_SELECT_BUTTON := true
+HISI_TARGET_PRODUCT := hi6210sft
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := hi6210sft
+TARGET_BOARD_PLATFORM_GPU := mali-450mp 
+TARGET_BOOTLOADER_BOARD_NAME := hi6210sft
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RPC := true
+TARGET_UNIFIED_DEVICE := true
+
+# Camera
+BOARD_CAMERA_HAVE_ISO := true
+COMMON_GLOBAL_CFLAGS += -DHAVE_ISO
+
+# CFLAGS
+#COMMON_GLOBAL_CFLAGS += -DHISILICON_HIHI6210SFT
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Chromium
+PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
+
+# EGL
+BOARD_EGL_CFG := device/huawei/hi6210sft/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+
+# Hardware
+BOARD_HAL_STATIC_LIBRARIES += libhealthd.hi6210sft
+
+# Kernel
+BOARD_KERNEL_CMDLINE := hisi_dma_print=0 vmalloc=384M maxcpus=8 no_irq_affinity androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x07478000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --tags_offset 0x02988000
+TARGET_PREBUILT_KERNEL := device/huawei/hi6210sft/kernel
+
+# LibC
+#BOARD_PROVIDES_ADDITIONAL_BIONIC_STATIC_LIBS += libc_huawei_symbols
+
+# Partitionsizes
+BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11605639168
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Preload bootanimation
+#TARGET_BOOTANIMATION_PRELOAD := true
+
+# Ramdisk
+TARGET_PROVIDES_INIT_RC := true
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_USE_CUSTOM_RECOVERY_FONT 	:= \"roboto_15x24.h\" 
+RECOVERY_FSTAB_VERSION := 2
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_RECOVERY_NEEDS_FBIOPAN_DISPLAY := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# SELinux
+#BOARD_SEPOLICY_DIRS += \
+#    device/huawei/hi6210sft/sepolicy
+#
+#BOARD_SEPOLICY_UNION += \
+#	file.te \
+#	genfs_contexts \
+#	kernel.te 
+
+# WEBGL in WebKit
+#ENABLE_WEBGL := true
+
+# TWRP specific flags
+# DEVICE_RESOLUTION := 720x1280
+# HAVE_SELINUX := true
+# TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd_backlight0/brightness"
+# TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/bq_bci_battery.1/power_supply/Battery"
+# TW_MAX_BRIGHTNESS := 255
+# TW_NO_USB_STORAGE := true
+# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/huawei/hi6210sft/graphics.c
---
+++ hi6210sft.mk
@@ -0,0 +1,114 @@
+# Android Open Source Project Base System
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Android Packages
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory \
+	libbt-vendor \
+	libc2dcolorconvert \
+	libdashplayer \
+	librs_jni \
+	libsrec_jni \
+	LiveWallpapers \
+	LiveWallpapersPicker \
+	make_ext4fs \
+	minivold \
+	runtime_libart_default \
+	setup_fs \
+	uim-sysfs \
+	UsbSpeedSwitch \
+	VisualizationWallpapers
+
+# AAPT Settings
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Blobs
+$(call inherit-product-if-exists, vendor/huawei/hi6210sft/hi6210sft-vendor.mk)
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# BT
+$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
+
+# Chromium 
+PRODUCT_COPY_FILES := \
+	device/huawei/hi6210sft/chromium/libwebviewchromium.so:system/lib/libwebviewchromium.so
+
+# Device Path
+LOCAL_PATH := device/huawei/hi6210sft
+
+# HiSilicon 6210 Packages
+PRODUCT_PACKAGES := \
+	camera.hi6210sft \
+	gralloc.hi6210sft \
+	hwcomposer.hi6210sft \
+	libGLES_android \
+	libGLES_mali \
+	keystore.hi6210sft \
+	memtrack.hi6210sft \
+	power.hi6210sft \
+	sensors.hi6210sft	
+
+# Kernel
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/huawei/hi6210sft/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Ramdisk & Recovery init.*.rc Files
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ramdisk/fstab.hi6210sft:root/fstab.hi6210sft \
+	$(LOCAL_PATH)/ramdisk/init:root/init \
+	$(LOCAL_PATH)/ramdisk/init.5801.rc:root/init.5801.rc \
+	$(LOCAL_PATH)/ramdisk/init.6165.rc:root/init.6165.rc \
+	$(LOCAL_PATH)/ramdisk/init.10106.rc:root/init.10106.rc \
+	$(LOCAL_PATH)/ramdisk/init.51054.rc:root/init.51054.rc \
+	$(LOCAL_PATH)/ramdisk/init.102173.rc:root/init.102173.rc \
+	$(LOCAL_PATH)/ramdisk/init.142782.rc:root/init.142782.rc \
+	$(LOCAL_PATH)/ramdisk/init.audio.rc:root/init.audio.rc \
+	$(LOCAL_PATH)/ramdisk/init.chip.usb.rc:root/init.chip.usb.rc \
+	$(LOCAL_PATH)/ramdisk/init.connectivity.bcm43xx.rc:root/init.connectivity.bcm43xx.rc \
+	$(LOCAL_PATH)/ramdisk/init.connectivity.hi110x.rc:root/init.connectivity.hi110x.rc \
+	$(LOCAL_PATH)/ramdisk/init.connectivity.rc:root/init.connectivity.rc \
+	$(LOCAL_PATH)/ramdisk/init.device.rc:root/init.device.rc \
+	$(LOCAL_PATH)/ramdisk/init.environ.rc:root/init.environ.rc \
+	$(LOCAL_PATH)/ramdisk/init.extmodem.rc:root/init.extmodem.rc \
+	$(LOCAL_PATH)/ramdisk/init.hi6210sft.rc:root/init.hi6210sft.rc \
+	$(LOCAL_PATH)/ramdisk/init.hisi.rc:root/init.hisi.rc \
+	$(LOCAL_PATH)/ramdisk/init.manufacture.rc:root/init.manufacture.rc \
+	$(LOCAL_PATH)/ramdisk/init.performance.rc:root/init.performance.rc \
+	$(LOCAL_PATH)/ramdisk/init.platform.rc:root/init.platform.rc \
+	$(LOCAL_PATH)/ramdisk/init.protocol.rc:root/init.protocol.rc \
+	$(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
+	$(LOCAL_PATH)/ramdisk/init.recovery.hi110x.rc:root/init.recovery.hi110x.rc \
+	$(LOCAL_PATH)/ramdisk/init.recovery.hi6210sft.rc:root/init.recovery.hi6210sft.rc \
+	$(LOCAL_PATH)/ramdisk/init.tee.rc:root/init.tee.rc \
+	$(LOCAL_PATH)/ramdisk/init.trace.rc:root/init.trace.rc \
+	$(LOCAL_PATH)/ramdisk/init.usb.rc:root/init.usb.rc \
+	$(LOCAL_PATH)/ramdisk/init.zygote32.rc:root/init.zygote32.rc \
+	$(LOCAL_PATH)/ramdisk/init.zygote64_32.rc:root/init.zygote64_32.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.5801.rc:root/ueventd.5801.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.6165.rc:root/ueventd.6165.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.10106.rc:root/ueventd.10106.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.51054.rc:root/ueventd.51054.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.102173.rc:root/ueventd.102173.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.142782.rc:root/ueventd.142782.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.rc:root/ueventd.rc 
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_hi6210sft
+PRODUCT_DEVICE := hi6210sft

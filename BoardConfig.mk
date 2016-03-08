e2fe5bae1fadeeac75e1626985743bee68e5f711
hak86 <hakamelassouad@gmail.com>
2016-03-08 16:29:34 +0100

    P8L: changes for L

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,72 +1,56 @@
-USE_CAMERA_STUB := true
-
-# inherit from the proprietary version
--include vendor/huawei/hi6210sft/BoardConfigVendor.mk
-
-# 64 Bit
+# 64 Bit Support
 ANDROID_64=true
-TARGET_IS_64_BIT := true
-TARGET_USES_64_BIT_BINDER := true
-TARGET_USES_HISI_DTIMAGE := true
+ANDROID_ENABLE_RENDERSCRIPT := true
 TARGET_SUPPORTS_32_BIT_APPS := true
 TARGET_SUPPORTS_64_BIT_APPS := true
-
-# ADB
-ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
-ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
-ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mass_storage
+TARGET_USES_64_BIT_BINDER := true
 
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
 TARGET_CPU_SMP := true
-TARGET_CPU_CORTEX_A53 := true
+TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
+TARGET_2ND_CPU_VARIANT := cortex-a15
 
 # Board
+BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-HISI_TARGET_PRODUCT := hi6210sft
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := kirin930
-TARGET_BOARD_PLATFORM_GPU := mali-450mp 
-TARGET_BOARD_SUFFIX := _64
-TARGET_BOOTLOADER_BOARD_NAME := p8litekirin
-TARGET_NO_RADIOIMAGE := true
-TARGET_NO_RPC := true
-TARGET_UNIFIED_DEVICE := true
+TARGET_BOARD_GPU := mali-450mp
+TARGET_BOARD_PLATFORM := hi6210sft
+TARGET_BOOTLOADER_BOARD_NAME := hi6210sft
+TARGET_HARDWARE_3D := true
+TARGET_USES_HISI_DTIMAGE := true
+TARGET_USERIMAGES_USE_EXT4 := true
+USE_OPENGL_RENDERER := true
+
+# Blobs
+-include vendor/huawei/hi6210sft/BoardConfigVendor.mk
+
+# Camera
+USE_CAMERA_STUB := true
 
 # Chromium
 PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
 
-# EGL
-BOARD_EGL_CFG := device/huawei/hi6210sft/configs/egl.cfg
-USE_OPENGL_RENDERER := true
-
-# Hardware
-BOARD_HAL_STATIC_LIBRARIES += libhealthd.hi6210sft
-
 # Kernel
-BOARD_KERNEL_CMDLINE := hisi_dma_print=0 vmalloc=384M maxcpus=8 no_irq_affinity androidboot.selinux=enforcing
 BOARD_KERNEL_BASE := 0x07478000
+BOARD_KERNEL_CMDLINE := hisi_dma_print=0 vmalloc=384M maxcpus=8 no_irq_affinity androidboot.selinux=enforcing
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --tags_offset 0x02988000
-TARGET_KERNEL_CONFIG := hisi_hi6210sft_64_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_hi6210sft_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_USES_UNCOMPRESSED_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/huawei/hi6210sft
 
-# Partitionsizes
+# Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
@@ -74,23 +58,9 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 11605639168
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Ramdisk
+TARGET_PROVIDES_INIT := true
 TARGET_PROVIDES_INIT_RC := true
 
 # Recovery
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_USE_CUSTOM_RECOVERY_FONT 	:= \"roboto_15x24.h\" 
-RECOVERY_FSTAB_VERSION := 2
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_SDCARD_ON_DATA := true
-BOARD_RECOVERY_NEEDS_FBIOPAN_DISPLAY := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_USERIMAGES_USE_EXT4 := true
-
-# TWRP specific flags
-# DEVICE_RESOLUTION := 720x1280
-# HAVE_SELINUX := true
-# TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd_backlight0/brightness"
-# TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/bq_bci_battery.1/power_supply/Battery"
-# TW_MAX_BRIGHTNESS := 255
-# TW_NO_USB_STORAGE := true
-# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/huawei/hi6210sft/graphics.c
--- hi6210sft.mk
+++ hi6210sft.mk
@@ -1,7 +1,8 @@
-# Android Open Source Project Base System
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+# Android Open Source Base System
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, build/target/product/full.mk)
 
 # Android Packages
 PRODUCT_PACKAGES += \
@@ -9,18 +10,27 @@ PRODUCT_PACKAGES += \
 	audio.r_submix.default \
 	audio.usb.default \
 	com.android.future.usb.accessory \
+	gatord \
+	hwclock \
+	iontest \
+	ion-unit-tests \
 	libbt-vendor \
 	libc2dcolorconvert \
 	libdashplayer \
+	libion \
 	librs_jni \
 	libsrec_jni \
 	LiveWallpapers \
 	LiveWallpapersPicker \
-	make_ext4fs \
-	minivold \
 	runtime_libart_default \
-	setup_fs \
-	uim-sysfs \
+	scp \
+	sftp \
+	ssh \
+	ssh-keygen \
+	start-ssh \
+	sshd_config \
+	sshd \
+	uim wpa_supplicant \
 	UsbSpeedSwitch \
 	VisualizationWallpapers
 
@@ -28,25 +38,42 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+
 # Blobs
 $(call inherit-product-if-exists, vendor/huawei/hi6210sft/hi6210sft-vendor.mk)
+
+# BT
+$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# BT
-$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
+# Charger
+PRODUCT_PACKAGES += \
+	charger \
+	charger_res_images
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/charger/battery_fail.png:root/res/images/charger/battery_fail.png \
+	$(LOCAL_PATH)/charger/battery_scale.png:root/res/images/charger/battery_scale.png
 
 # Chromium 
 PRODUCT_COPY_FILES := \
-	device/huawei/hi6210sft/chromium/libwebviewchromium.so:system/lib/libwebviewchromium.so
+	$(LOCAL_PATH)/chromium/libwebviewchromium.so:system/lib/libwebviewchromium.so
 
 # Device Path
 LOCAL_PATH := device/huawei/hi6210sft
 
+# File System
+PRODUCT_PACKAGES += \
+	e2fsck \
+	make_ext4fs \
+	minivold \
+	setup_fs 
+
 # HiSilicon 6210 Packages
-PRODUCT_PACKAGES := \
+PRODUCT_PACKAGES += \
 	camera.hi6210sft \
 	gralloc.hi6210sft \
 	hwcomposer.hi6210sft \
@@ -57,6 +84,50 @@ PRODUCT_PACKAGES := \
 	power.hi6210sft \
 	sensors.hi6210sft
 
+# Kernel, My Own Cannabis :D 
+# ifeq ($(TARGET_PREBUILT_KERNEL),)
+# 	LOCAL_KERNEL := device/huawei/hi6210sft/kernel
+# else
+# 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+# endif
+
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_KERNEL):kernel
+
+# keylayouts
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+	$(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
+	$(LOCAL_PATH)/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_0b05_Product_4500.kl:system/usr/keylayout/Vendor_0b05_Product_4500.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1bad_Product_f016.kl:system/usr/keylayout/Vendor_1bad_Product_f016.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1bad_Product_f023.kl:system/usr/keylayout/Vendor_1bad_Product_f023.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1bad_Product_f027.kl:system/usr/keylayout/Vendor_1bad_Product_f027.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1bad_Product_f036.kl:system/usr/keylayout/Vendor_1bad_Product_f036.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1d79_Product_0009.kl:system/usr/keylayout/Vendor_1d79_Product_0009.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_05ac_Product_0239.kl:system/usr/keylayout/Vendor_05ac_Product_0239.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_12bd_Product_d015.kl:system/usr/keylayout/Vendor_12bd_Product_d015.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_18d1_Product_2c40.kl:system/usr/keylayout/Vendor_18d1_Product_2c40.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_22b8_Product_093d.kl:system/usr/keylayout/Vendor_22b8_Product_093d.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_028e.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c21d.kl:system/usr/keylayout/Vendor_046d_Product_c21d.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c21f.kl:system/usr/keylayout/Vendor_046d_Product_c21f.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c216.kl:system/usr/keylayout/Vendor_046d_Product_c216.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c219.kl:system/usr/keylayout/Vendor_046d_Product_c219.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c294.kl:system/usr/keylayout/Vendor_046d_Product_c294.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c299.kl:system/usr/keylayout/Vendor_046d_Product_c299.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_046d_Product_c532.kl:system/usr/keylayout/Vendor_046d_Product_c532.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_054c_Product_0268.kl:system/usr/keylayout/Vendor_054c_Product_0268.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_0079_Product_0011.kl:system/usr/keylayout/Vendor_0079_Product_0011.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_0583_Product_2060.kl:system/usr/keylayout/Vendor_0583_Product_2060.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1038_Product_1412.kl:system/usr/keylayout/Vendor_1038_Product_1412.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1689_Product_fd00.kl:system/usr/keylayout/Vendor_1689_Product_fd00.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1689_Product_fd01.kl:system/usr/keylayout/Vendor_1689_Product_fd01.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1689_Product_fe00.kl:system/usr/keylayout/Vendor_1689_Product_fe00.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_1949_Product_0401.kl:system/usr/keylayout/Vendor_1949_Product_0401.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_2378_Product_100a.kl:system/usr/keylayout/Vendor_2378_Product_100a.kl \
+	$(LOCAL_PATH)/keylayout/Vendor_2378_Product_1008.kl:system/usr/keylayout/Vendor_2378_Product_1008.kl 
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
@@ -64,7 +135,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml	
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += device/huawei/hi6210sft/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -87,8 +158,21 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-# Ramdisk & Recovery init.*.rc Files
+# Ramdisk
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ramdisk/sbin/adbd:root/sbin/adbd \
+	$(LOCAL_PATH)/ramdisk/sbin/check_root:root/sbin/check_root \
+	$(LOCAL_PATH)/ramdisk/sbin/e2fsck_s:root/sbin/e2fsck_s \
+	$(LOCAL_PATH)/ramdisk/sbin/emmc_partation:root/sbin/emmc_partation \
+	$(LOCAL_PATH)/ramdisk/sbin/hdbd:root/sbin/hdbd \
+	$(LOCAL_PATH)/ramdisk/sbin/healthd:root/sbin/healthd \
+	$(LOCAL_PATH)/ramdisk/sbin/kmsgcat:root/sbin/kmsgcat \
+	$(LOCAL_PATH)/ramdisk/sbin/logctl_service:root/sbin/logctl_service \
+	$(LOCAL_PATH)/ramdisk/sbin/ntfs-3gd:root/sbin/ntfs-3gd \
+	$(LOCAL_PATH)/ramdisk/sbin/oeminfo_nvm_server:root/sbin/oeminfo_nvm_server \
+	$(LOCAL_PATH)/ramdisk/sbin/teecd:root/sbin/teecd \
+	$(LOCAL_PATH)/ramdisk/sbin/ueventd:root/sbin/ueventd \
+	$(LOCAL_PATH)/ramdisk/sbin/watchdogd:root/sbin/watchdogd/ \
 	$(LOCAL_PATH)/ramdisk/fstab.hi6210sft:root/fstab.hi6210sft \
 	$(LOCAL_PATH)/ramdisk/init:root/init \
 	$(LOCAL_PATH)/ramdisk/init.5801.rc:root/init.5801.rc \
@@ -126,8 +210,15 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/ramdisk/ueventd.102173.rc:root/ueventd.102173.rc \
 	$(LOCAL_PATH)/ramdisk/ueventd.142782.rc:root/ueventd.142782.rc \
 	$(LOCAL_PATH)/ramdisk/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.rc:root/ueventd.rc 
+	$(LOCAL_PATH)/ramdisk/ueventd.rc:root/ueventd.rc
+
+# Sepolicy Configuration
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/sepolicy/file_contexts:root/file_contexts \
+	$(LOCAL_PATH)/sepolicy/property_contexts:root/property_contexts \
+	$(LOCAL_PATH)/sepolicy/seapp_contexts:root/seapp_contexts \
+	$(LOCAL_PATH)/sepolicy/service_contexts:root/service_contexts 
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_hi6210sft
+PRODUCT_NAME := cm_hi6210sft
 PRODUCT_DEVICE := hi6210sft
